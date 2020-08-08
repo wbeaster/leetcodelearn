684e189a883b22575a29f39a25ba35d0935de18c
Will <williambeaster@gmail.com>
2020-08-08 17:07:56 +0900

    solution cadidate

---
+++ sortedSquares.js
@@ -0,0 +1,44 @@
+/*
+Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+
+Example 2:
+
+Input: [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+
+ 
+
+Note:
+
+    1 <= A.length <= 10000
+    -10000 <= A[i] <= 10000
+    A is sorted in non-decreasing order.
+
+*/
+
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortedSquares = function(A) 
+{
+    let B = Int16Array.from(A);
+
+    for (let i = 0; i < B.length; i++) 
+        B[i] = B[i] * B[i];
+
+    B.sort();
+
+    return B;
+};
+
+let data = [-7,-3,2,3,11];
+
+console.log(sortedSquares(data));
