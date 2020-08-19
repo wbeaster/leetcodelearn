f06bb1e5a863586811049060a8b72f9635eaec56
Will <williambeaster@gmail.com>
2020-08-19 23:16:13 +0900

    solution #12

--- 12.thirdMax.js
+++ 12.thirdMax.js
@@ -34,40 +34,33 @@ Both numbers with value 2 are both considered as second maximum.
  */
 var thirdMax = function(nums) 
 {
-    max1 = nums[0];
-    max2 = nums[0];
-    max3 = nums[0];
+    let max1 = nums[0];
+    let max2 = nums[0];
+    let max3 = nums[0];
 
     for (let i = 0; i < nums.length; i++)
-    {
-        if (nums[i] > max3)
+        if (nums[i] < nums[0])
         {
-            if (nums[i]> max2)
-            {
-                if (nums[i] > max1)
-                {
-                    max3 = max2;
-                    max2 = max1;
-                    max1 = nums[i]
-                }
-                else
-                {
-                    max3 = max2;
-                    max2 = nums[i];
-                }
-            }
-            else
-                max3 = nums[i];
+            max1 = nums[i];
+            max2 = nums[i];
+            max3 = nums[i];
         }
-    }
+
+    for (let i = 0; i < nums.length; i++)
+        if (nums[i] > max1)
+            max1 = nums[i];
+
+    for (let i = 0; i < nums.length; i++)
+        if (nums[i] != max1 && nums[i] > max2)
+            max2 = nums[i];
+
+    for (let i = 0; i <nums.length; i++)
+        if (nums[i] != max1 && nums[i] != max2 && nums[i] > max3)
+            max3 = nums[i];
 
     if (max1 > max2 && max2 > max3) return max3;
     
     return (max2 > max1) ? (max2) : (max1);
-
-
-
-    
 };
 
 let data = [
@@ -131,6 +124,26 @@ let data = [
     {
         nums: [0,0,0,2,1,2,2,3,0,4,2],
         val:  [2,1,2,2,3,4,2,0,0,0,0]
+    },
+    {
+        nums: [3, 2, 1],
+        val:  1
+    },
+    {
+        nums: [1, 2],
+        val:  2
+    },
+    {
+        nums: [2, 2, 3, 1],
+        val:  1
+    },
+    {
+        nums: [-1, -2],
+        val:  2
+    },
+    {
+        nums: [-2, -2, -3, -1, -4, -5, 0],
+        val:  1
     }
 ]
 
